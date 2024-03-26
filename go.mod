8aad249471fa950e8d3ec53a36021ec79d239359
dchernenko <dim.chernenko@gmail.com>
2024-03-26 05:12:09 +0200

    rename

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/den-vasyliev/kbot
+module github.com/diamonce/2nd_week_vc
 
 go 1.19
 
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/den-vasyliev/kbot/cmd"
+import "github.com/diamonce/2nd_week_vcs/cmd"
 
 func main() {
 	cmd.Execute()
